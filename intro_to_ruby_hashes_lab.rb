9fcfb152e2aca8bd143aec316961908eee2a33eb
Michael Waldo <mbwcogic@yahoo.com>
2020-01-05 20:15:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,9 +30,9 @@ monopoly = {
 	 :railroads => {
 	   :pieces => 4, 
 	   :names => {
-	     :reading_railroad=>{}
-	     :pennsylvania_railroad=>{} 
-	     :b_and_o_railroad=>{}
+	     :reading_railroad=>{},
+	     :pennsylvania_railroad=>{}, 
+	     :b_and_o_railroad=>{},
 	     :shortline=>{}
 	   },
 	   :rent_in_dollars => {
