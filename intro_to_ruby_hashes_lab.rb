754d9905b67618acae7d4b9de5a6c082266e13c8
Michael Waldo <mbwcogic@yahoo.com>
2020-01-05 20:16:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -65,5 +65,5 @@ def monopoly_with_fourth_tier
 	 }
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 }#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-}
+
 end
